66cbe1c3ca1a552668478669297b94011797f775
Michael O <michaelolsen8@MacBook-Pro.local>
2022-06-19 23:02:57 -0600

    radio buttons display correct choices on each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,166 +3,166 @@
 var questions = [
     {
         question: "1. Against what team did John Stockton hit a game-winning three to send the Jazz to the NBA Finals?",
-        answers: {
-            a: "Houston Rockets",
-            b: "LA Lakers",
-            c: "Chicago Bulls",
-            d: "Seattle Sonics"
-        },
+        answers: [
+            "Houston Rockets",
+             "LA Lakers",
+             "Chicago Bulls",
+             "Seattle Sonics",
+        ],
         correctAnswer: "Houston Rockets"
 
     },
     {
         question: "2. Deron Williams was drafted out of which college in 2005?",
-        answers: {
-            a: "West Virginia",
-            b: "Texas",
-            c: "Cal-Berkely",
-            d: "Illinois"
-        },
+        answers: [
+             "West Virginia",
+             "Texas",
+             "Cal-Berkely",
+             "Illinois"
+        ],
         correctAnswer: "Illinois"
 
     },
     {
         question: "3. Who did the Jazz recieve from Phoenix along with 2 first round picks that eventually led to Utah drafting Gordon Hayward?",
-        answers: {
-            a: "Keon Clark",
-            b: "Tom Gugliotta",
-            c: "Raja Bell",
-            d: "Glen Rice"
-        },
+        answers: [
+             "Keon Clark",
+             "Tom Gugliotta",
+             "Raja Bell",
+             "Glen Rice"
+        ],
         correctAnswer: "Tom Gugliotta"
 
     },
     {
         question: "4. Which team did the pick originally belong to that Utah used to draft Gordon Hayward?",
-        answers: {
-            a: "Knicks",
-            b: "Thunder",
-            c: "Celtics",
-            d: "Lakers"
-        },
+        answers: [
+             "Knicks",
+             "Thunder",
+             "Celtics",
+             "Lakers"
+        ],
         correctAnswer: "Knicks"
 
     },
     {
         question: "5. Who did the Jazz beat in the conference semifinals to make it to the Western Conference Finals in 2006-07 before eventually losing to the San Antonio Spurs?",
-        answers: {
-            a: "Lakers",
-            b: "Sonics",
-            c: "Warriors",
-            d: "Clippers"
-        },
+        answers: [
+             "Lakers",
+             "Sonics",
+             "Warriors",
+             "Clippers"
+        ],
         correctAnswer: "Clippers"
 
     },
     {
         question: "6. Who did the Jazz draft with the 3rd overall pick in 1982?",
-        answers: {
-            a: "Danny Ainge",
-            b: "James Worthy",
-            c: "Dominique Wilkins",
-            d: "Karl Malone"
-        },
+        answers: [
+             "Danny Ainge",
+             "James Worthy",
+             "Dominique Wilkins",
+             "Karl Malone"
+        ],
         correctAnswer: "Dominique Wilkins"
 
     },
     {
         question: "7. Which player has the highest 2 point percentage in Jazz history?",
-        answers: {
-            a: "Karl Malone",
-            b: "Rudy Gobert",
-            c: "Mark Eaton",
-            d: "Jeff Hornacek"
-        },
+        answers: [
+             "Karl Malone",
+             "Rudy Gobert",
+             "Mark Eaton",
+             "Jeff Hornacek"
+        ],
         correctAnswer: "Rudy Gobert"
 
     },
     {
         question: "8. What college did Jazz cult-hero Sundiata Gaines attend?",
-        answers: {
-            a: "Florida State",
-            b: "Kennesaw",
-            c: "Iowa",
-            d: "Georgia"
-        },
+        answers: [
+             "Florida State",
+             "Kennesaw",
+             "Iowa",
+             "Georgia"
+        ],
         correctAnswer: "Georgia"
 
     },
     {
         question: "9. How many straight seasons did John Stockton lead the NBA in assists per game?",
-        answers: {
-            a: "8",
-            b: "4",
-            c: "11",
-            d: "5"
-        },
+        answers: [
+             "8",
+             "4",
+             "11",
+             "5"
+        ],
         correctAnswer: "8"
 
     },
     {
         question: "10. What team did the Utah Jazz trade Jeff Malone to in return for Jeff Hornacek?",
-        answers: {
-            a: "Suns",
-            b: "Knicks",
-            c: "76ers",
-            d: "Warriors"
-        },
+        answers: [
+             "Suns",
+             "Knicks",
+             "76ers",
+             "Warriors"
+        ],
         correctAnswer: "76ers"
 
     },
     {
         question: "11. Who did the Jazz draft at number 24 for the Denver Nuggets for the draft rights to Donovan Mitchell?",
-        answers: {
-            a: "Dennis Smith, Jr.",
-            b: "Tyler Lydon",
-            c: "Trey Lyles",
-            d: "Jamal Murray"
-        },
+        answers: [
+             "Dennis Smith, Jr.",
+             "Tyler Lydon",
+             "Trey Lyles",
+             "Jamal Murray"
+        ],
         correctAnswer: "Tyler Lydon"
 
     },
     {
         question: "12. The Jazz hired current head coach, Quin Snyder, from which team?",
-        answers: {
-            a: "Spurs",
-            b: "University of Missouri",
-            c: "Hawks",
-            d: "CSKA Moscow"
-        },
+        answers: [
+             "Spurs",
+             "University of Missouri",
+             "Hawks",
+             "CSKA Moscow"
+        ],
         correctAnswer: "Hawks"
 
     },
     {
         question: "13. Who was arrested, along with Deron Williams, in Park City and gave the name Bobby Williams to the police?",
-        answers: {
-            a: "Robert Whaley",
-            b: "Dee Brown",
-            c: "Paul Millsap",
-            d: "CJ Miles"
-        },
+        answers: [
+             "Robert Whaley",
+             "Dee Brown",
+             "Paul Millsap",
+             "CJ Miles"
+        ],
         correctAnswer: "Robert Whaley"
 
     },
     {
         question: "14. What name did Deron Williams give to police when questioned at the scene?",
-        answers: {
-            a: "Adam Silver",
-            b: "Torrey Ellis",
-            c: "Bobby Williams",
-            d: "Ricky Bobby"
-        },
+        answers: [
+             "Adam Silver",
+             "Torrey Ellis",
+             "Bobby Williams",
+             "Ricky Bobby"
+        ],
         correctAnswer: "Torrey Ellis"
 
     },
     {
         question: "15. What former player infamously cried in an interview during a playoff series against the Houston Rockets?",
-        answers: {
-            a: "Greg Ostertag",
-            b: "Derek Fisher",
-            c: "Mehmet Okur",
-            d: "Andrei Kirilenko"
-        },
+        answers: [
+             "Greg Ostertag",
+             "Derek Fisher",
+             "Mehmet Okur",
+             "Andrei Kirilenko"
+        ],
         correctAnswer: "Andrei Kirilenko"
 
     },
@@ -177,61 +177,45 @@ setupPage();
 
 
 function setupPage() {
+    let num = 0;
     for ( var i = 0; i < questions.length; i++ ) {
         var p = $( "<p>" );
         $( p ).attr( "questionsdiv", "question" + i );
         $( p ).attr( "data-number", i );
         $( p ).html( "<br>" + questions[i].question + "<br>" );
         $( "#questionsdiv" ).append( p );
-        // console.log( questions[i].question );
-        {
-            for ( let i = 0; i < questions[i].answers.length; i++ )
-                console.log(questions[i])
-                correctAnswers.push( questions[i].correctAnswer )
-                 {
-                
-
-                //button for "a" choice
-                var a = $( "<button>" );
-                $( a ).attr( "data-choices" + i );
-                $( a ).attr( "answers", i );
-                $( a ).text( questions[i].answers );
-                $( "#questionsdiv" ).append( a );
-                // console.log( questions[i].answers );
-                a.text( questions[i].answers.a )
-
-                //button for "b" choice
-                var b = $( "<button>" );
-                $( b ).attr( "data-choices" + i );
-                $( b ).attr( "answers", i );
-                $( "#questionsdiv" ).append( b );
-                $( b ).text( questions[i].answers );
-                b.text( questions[i].answers.b )
-                // console.log( b.text( questions[i].answers.b ) );
-
-                //button for "c" choice
-                var c = $( "<button>" );
-                $( c ).attr( "data-choices" + i );
-                $( c ).attr( "answers", i );
-                $( "#questionsdiv" ).append( c );
-                $( c ).text( questions[i].answers );
-                c.text( questions[i].answers.c )
-
-                //button for "d" choice
-                var d = $( "<button>" );
-                $( d ).attr( "data-choices" + i );
-                $( d ).attr( "answers", i );
-                $( "#questionsdiv" ).append( d );
-                $( d ).text( questions[i].answers );
-                d.text( questions[i].answers.d )
-            }
+        // correctAnswers.push( questions[i].correctAnswer )
+        let array = [];
+        
+        for ( let i = 0; i < 4; i++ ) {
+                array.push(questions[num].answers[i])
+                // console.log( array )
+            
+          $( '#questionsdiv' ).append(
+                    $( '<input>' ).prop( {
+                        type: 'radio',
+                        id: questions[num].answers[i],
+                        name: questions[num].answers[i],
+                        value: questions[num].answers[i],
+                        // checked: unchecked
+                    } )).append( $( '<label>' ).prop( {
+                     for: questions[num].answers[i]
+                    } ).html(questions[num].answers[i])
+         ).append( $( '<br>' ) );
+            
         }
-
-    }
-
+        
+       
+       num=num+1         
+     }
+        
+    
 }
 
-console.log(correctAnswers)
+    
+
+
+// console.log(correctAnswers)
 
 
 
@@ -271,24 +255,20 @@ var userAnswers = [];
 
 
 
-$( "button" ).on( "click", function ( event ) {
+$( "input" ).on( "click", function ( event ) {
 
-
-    console.log( event.currentTarget )
+    // $( "#answers-div" ).attr( "answers" );
     $( "#answers-div" ).attr( "answers" );
-    $( "#answers-div" ).attr( "answers" );
-    $( event.currentTarget ).css( "background-color", "red" );
+    // console.log( event )
+    // $( event.currentTarget ).css( "background-color", "red" );
 
     
     // $( "#answers-div" ).append( "<h6> " + event.currentTarget.innerHTML + "</h6>" );
 
-   
     userAnswers.push( event.currentTarget.innerHTML );
-    console.log( userAnswers );
+    console.log( event.currentTarget.value );
     // $( "#answers-div" ).text( "<h6>" + event.currentTarget.innerHTML + "</h6>" );
-}
-
-);
+});
 
 let wrong = 0;
 
@@ -300,7 +280,8 @@ $( "#submitButton" ).on( "click", function () {
         }
         wrong=wrong
     }
-    alert(wrong)
+    console.log( userAnswers );
+    console.log(correctAnswers)
 })
 
 
